69b8dfd48126dafa04bd92610ab13983433f963c
Safronov Ivan <50119705+SidNotSus@users.noreply.github.com>
2019-10-30 09:38:53 +0300

    Finaly working algorithms
    For Ilya Bychkov

--- shesrch/shesrch.cpp
+++ shesrch/shesrch.cpp
@@ -4,36 +4,38 @@
 
 using namespace std;
 
-void BinarySearch(vector<int> arr,int left,int right, int num,bool flag)
-{
-	
+/////////////////////////////////////////////////////////////
+//1 TEST 7 32 1 2 3 4 5 15 32
+//2 TEST 16 10 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+//
+/////////////////////////////////////////////////////////////
 
-	
+
+void BinarySearch(vector<int> arr, int left, int right, int num, bool flag)
+{
+
+
+
 	int mid;
 	if (flag == true)
 	{
-		if (right >= arr.size()) right = arr.size() - 1;
+		if (right >= arr.size()) 
+			right = arr.size() - 1;
 	}
-	
-	for (size_t i = 0; i < arr.size(); i++)
+
+	while (left <= right)
 	{
 		mid = (left + right) / 2;
 		cout << arr[mid] << " ";
 		if (arr[mid] < num)
 		{
-			left = mid+1;
-			//arr.erase(arr.begin() + left, arr.begin() + mid);
+			left = mid + 1;
 		}
 		if (arr[mid] > num)
 		{
-			right = mid-1;
-			//arr.erase(arr.begin() + mid, arr.begin() + right);
+			right = mid - 1;
 		}
-		if (arr[mid] == num || /*(mid== left &&*/ mid ==right)
-		{
-			break;
-		}
-		if (flag ==0 && left == right)
+		if (arr[mid] == num)
 		{
 			break;
 		}
@@ -43,24 +45,24 @@ void BinarySearch(vector<int> arr,int left,int right, int num,bool flag)
 
 void DoublingSearch(vector<int> arr, int num)
 {
-int i = 0;
-while (pow(2, i) < arr.size() && num > arr[pow(2, i)])
-{
-	i -= -1;
-}
-if (pow(2, i) < arr.size() && arr[pow(2, i)] == num)
-{
-	cout << num;
-	return;
-}
-bool flag = true;
-if (i > 0)
-if (pow(2, i) < arr.size())
-	BinarySearch(arr, pow(2, i - 1) + 1, pow(2, i) - 1, num, true);
-else
-BinarySearch(arr, pow(2, i - 1) + 1, pow(2, i), num, true);
-else
-BinarySearch(arr, 0, 1, num, true);
+	int i = 0;
+	while (pow(2, i) < arr.size() && num > arr[pow(2, i)])
+	{
+		i -= -1;
+	}
+	if (pow(2, i) < arr.size() && arr[pow(2, i)] == num)
+	{
+		cout << num;
+		return;
+	}
+	bool flag = true;
+	if (i > 0)
+		if (pow(2, i) < arr.size())
+			BinarySearch(arr, pow(2, i - 1) + 1, pow(2, i) - 1, num, true);
+		else
+			BinarySearch(arr, pow(2, i - 1) + 1, pow(2, i), num, true);
+	else
+		BinarySearch(arr, 0, 1, num, true);
 }
 
 int main()
@@ -94,7 +96,7 @@ int main()
 	}
 
 	cout << "\nBinary search:\n";
-	BinarySearch(arr, 0, arr.size() ,num, false);
+	BinarySearch(arr, 0, arr.size()-1, num, false);
 	cout << "\nDoubling search:\n";
 	DoublingSearch(arr, num);
 
@@ -102,4 +104,3 @@ int main()
 
 	return(0);
 }
-
